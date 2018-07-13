067bfb50bda32a35ca13e2f6e43b8283f7790d0d
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-07-14 05:17:02 +0530

    iteration 1: 2m 45s

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,38 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HomePage());
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.edit,
+                color: Colors.white,
+              ),
+              onPressed: () {},
+            )
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: null,
+          child: Icon(
+            Icons.star,
+            color: Colors.yellow,
+          ),
+        ),
+        drawer: Drawer(),
+      ),
+    );
+  }
+}
+
+//Iteration 1 took 2m 45s
